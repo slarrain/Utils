da3795e3c2e0526e894ac47cb47bcc1b762d450f
Santiago Larrain <slarrain@uchicago.edu>
2019-06-10 15:08:35 -0400

    Add smartfinancial

--- scripts/db_conn_alive.sh
+++ scripts/db_conn_alive.sh
@@ -8,18 +8,26 @@ declare -A dbs=(
     [CERT_Internacional]=false
     [CERT_Platinum]=false
     [CERT_Trial]=false
+    [CERT_SmartFinancial]=false
 )
 # source db ini
 . ~/github/defontana2/smart_finance/credentials.ini 
 
 # Some harcoded variables
 CHECK_OK="(1 rows affected)"
-QUERY='select top 1 1 from dbo.docvtaenc'
+QUERY='select top 1 1 from dbo.'
 OK=":large_blue_circle:"
 BAD=":red_circle:"
+TABLE_DEFAULT="docvtaenc"
+TABLE_SMART="cesiones"
 
 for db in ${!dbs[@]}; do
-    rv=$(/opt/mssql-tools/bin/sqlcmd -U $user -P $password -S $host -d $db -Q "$QUERY")
+    if [[ $db == "CERT_SmartFinancial" ]]; then
+        table=$TABLE_SMART
+    else
+        table=$TABLE_DEFAULT
+    fi
+    rv=$(/opt/mssql-tools/bin/sqlcmd -U $user -P $password -S $host -d $db -Q "$QUERY$table")
     if [[ $rv == *"$CHECK_OK"* ]] 
     then
         dbs[$db]=$OK
