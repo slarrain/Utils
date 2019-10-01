8b4af724299dea67841ffc8689b1a6d130b2b751
Santiago Larrain <slarrain@uchicago.edu>
2019-10-01 19:35:18 -0300

    check exists 3

--- scripts/cel_camila.py
+++ scripts/cel_camila.py
@@ -10,12 +10,20 @@ def check_exists():
     my_files = glob(DEST_PREFIX + 'fotos/*/*/*') + glob(DEST_PREFIX + 'videos/*/*/*') 
     print ("All: {}\nMine: {}".format(len(all_files), len(my_files)))
 
-    result = [f.split('/')[0] for f in all_files if f in [g.split('/')[0] for g in my_files]]
+    result = [f.split('/')[-1] for f in all_files if f in [g.split('/')[-1] for g in my_files]]
     print (result)
+    return result, all_files, my_files
 
 
 def main():
-    check_exists()
+    result, all_files, my_files = check_exists()
+    # if len(result) > 0:
+    #     print ("ERROR. Archivos serán sobreescritos")
+    #     exit(1)
+    # for path in all_files:
+    #     filename = path.split('/')[-1]
+    #     tipo = 'fotos' if filename.startswith('IMG') 
+
 
 if __name__ == "__main__":
     main()
